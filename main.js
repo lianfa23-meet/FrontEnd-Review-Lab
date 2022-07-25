18896fbced7173abbf96784ce1ffd4335bfabc91
lianfa23-meet <109899264+lianfa23-meet@users.noreply.github.com>
2022-07-25 14:13:04 +0300

    t

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 var i=0;
+
+var song= new Audio('songrick.mp3')
+
 function hi(){
 	if(i==0){
 		document.getElementsByTagName('body')[0].style.backgroundColor = "lightblue";
+		document.body.style.backgroundImage = "url('rick.jpeg')"; 
+		song.play();
 		i=1;
 	}
 	else if(i==1){
 		document.getElementsByTagName('body')[0].style.backgroundColor = "lightseagreen";
+		document.body.style.backgroundImage = "none"; 
+		song.pause();
 		i=0;
 	}
 }
