83f5eaa80672dae608248b04fbd5b7a1ab49635f
lianfa23-meet <109899264+lianfa23-meet@users.noreply.github.com>
2022-07-25 14:02:04 +0300

    hh

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-function click(){
-document.body.style.color = "lightblue";
+var i=0;
+function hi(){
+	if(i==0){
+		document.getElementsByTagName('body')[0].style.backgroundColor = "lightblue";
+		i=1;
+	}
+	else if(i==1){
+		document.getElementsByTagName('body')[0].style.backgroundColor = "lightseagreen";
+		i=0;
+	}
 }
